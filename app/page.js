7c5dd8a86af3bce40c2e9fb383f0366633cfc1a2
Martin Sági <sagimrtin@gmail.com>
2023-10-10 17:29:33 +0200

    Add scoring and results display functionality

--- app/page.js
+++ app/page.js
@@ -6,12 +6,17 @@ export default function Home() {
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const [selectedOption, setSelectedOption] = useState("");
   const [showAnswer, setShowAnswer] = useState(false);
+  const [score, setScore] = useState(0);
+  const [showResults, setShowResult] = useState(false);
 
   const question = questions[currentQuestion];
 
   function handleOptionSelect(option) {
     setSelectedOption(option);
     setShowAnswer(true);
+    if (option === question.answer) {
+      setScore((prevScore) => prevScore + 1);
+    }
   }
 
   function handleNextQuestion() {
@@ -24,41 +29,51 @@ export default function Home() {
   }
 
   return (
-    <div className="quiz-app">
-      <div className="quiz-header">
-        <h1>Quiz App</h1>
-      </div>
-      <div className="quiz-body">
-        <h1>
-          {question.id}. {question.question}
-        </h1>
-        <div className="options">
-          {question.options.map((option) => {
-            return (
-              <button
-                className={
-                  showAnswer && option === question.answer
-                    ? "correct-answer"
-                    : showAnswer && option === selectedOption
-                    ? "wrong-answer"
-                    : ""
-                }
-                onClick={() => handleOptionSelect(option)}
-              >
-                {option}
-              </button>
-            );
-          })}
+    <>
+      {showResults ? (
+        <div className="quiz-app">
+          <h2>
+            You scored {score} out {questions.length}{" "}
+          </h2>
         </div>
-      </div>
-      <div className="quiz-footer">
-        <p>
-          {currentQuestion + 1} out of {questions.length}
-        </p>
-        <button onClick={handleNextQuestion} className="next">
-          Next
-        </button>
-      </div>
-    </div>
+      ) : (
+        <div className="quiz-app">
+          <div className="quiz-header">
+            <h1>Quiz App</h1>
+          </div>
+          <div className="quiz-body">
+            <h1>
+              {question.id}. {question.question}
+            </h1>
+            <div className="options">
+              {question.options.map((option) => {
+                return (
+                  <button
+                    className={
+                      showAnswer && option === question.answer
+                        ? "correct-answer"
+                        : showAnswer && option === selectedOption
+                        ? "wrong-answer"
+                        : ""
+                    }
+                    onClick={() => handleOptionSelect(option)}
+                  >
+                    {option}
+                  </button>
+                );
+              })}
+            </div>
+          </div>
+          <div className="quiz-footer">
+            <p>
+              {currentQuestion + 1} out of {questions.length}
+            </p>
+            <button onClick={handleNextQuestion} className="next">
+              Next
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
