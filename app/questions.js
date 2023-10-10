8f59f9df4ee70678d4e32cf60bd3c2f075f77f85
Martin Sági <sagimrtin@gmail.com>
2023-10-10 16:51:15 +0200

    Add questions data

---
+++ app/questions.js
@@ -0,0 +1,57 @@
+export const questions = [
+    {
+        id: 1,
+        question: "What did one wall say to the other wall?",
+        options: [
+            "I'll meet you at the corner.",
+            "Stop staring at me!",
+            "I'm board.",
+            "Let's hang out sometime.",
+        ],
+        answer: "I'll meet you at the corner.",
+    },
+    {
+        id: 2,
+        question: "Why don't scientists trust atoms?",
+        options: [
+            "They're too small.",
+            "They're always positive.",
+            "They make up everything.",
+            "They're too negative.",
+        ],
+        answer: "They make up everything.",
+    },
+    {
+        id: 3,
+        question: "How do you organize a space party?",
+        options: [
+            "You planet.",
+            "With a star-studded guest list.",
+            "By sending out invitations to the Milky Way.",
+            "Hire an astronaut DJ.",
+        ],
+        answer: "You planet.",
+    },
+    {
+        id: 4,
+        question: "What do you call a bear with no teeth?",
+        options: [
+            "A toothless bear.",
+            "A gummy bear.",
+            "A hungry bear.",
+            "A scary bear.",
+        ],
+        answer: "A gummy bear.",
+    },
+    {
+        id: 5,
+        question: "Why did the scarecrow win an award?",
+        options: [
+            "Because he had a great personality.",
+            "Because he was outstanding in his field.",
+            "Because he was very scary.",
+            "Because he could talk to the crows.",
+        ],
+        answer: "Because he was outstanding in his field.",
+    },
+];
