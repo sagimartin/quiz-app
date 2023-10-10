b72f771d915437cb5f551a3b4d53f79489d859de
Martin Sági <sagimrtin@gmail.com>
2023-10-10 17:19:34 +0200

    Add state management for quiz questions and answers

--- app/page.js
+++ app/page.js
@@ -1,16 +1,45 @@
+"use client";
+import { questions } from "./questions";
+import { useState } from "react";
+
 export default function Home() {
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [selectedOption, setSelectedOption] = useState("");
+  const [showAnswer, setShowAnswer] = useState(false);
+
+  const question = questions[currentQuestion];
+
+  function handleOptionSelect(option) {
+    setSelectedOption(option);
+    setShowAnswer(true);
+  }
+
   return (
     <div className="quiz-app">
       <div className="quiz-header">
         <h1>Quiz App</h1>
       </div>
       <div className="quiz-body">
-        <h1> 1. Question ere</h1>
+        <h1>
+          {question.id}. {question.question}
+        </h1>
         <div className="options">
-          <button>Answer 1</button>
-          <button>Answer 2</button>
-          <button>Answer 3</button>
-          <button>Answer 4</button>
+          {question.options.map((option) => {
+            return (
+              <button
+                className={
+                  showAnswer && option === question.answer
+                    ? "correct-answer"
+                    : showAnswer && option === selectedOption
+                    ? "wrong-answer"
+                    : ""
+                }
+                onClick={() => handleOptionSelect(option)}
+              >
+                {option}
+              </button>
+            );
+          })}
         </div>
       </div>
       <div className="quiz-footer">
